4f341d723f0009500493c27c964d457e4e672f3f
anfly <zyanfly1832@163.com>
2019-07-11 00:39:36 +0800

    完成merchant-images

--- pages/merchant-images/index.js
+++ pages/merchant-images/index.js
@@ -7,6 +7,7 @@ const merchantImageModel = new MerchantImageModel()
 Page({
     data: {
         merchant_images: null,
+        showImageList: null,
         loadingCenter: true
     },
 
@@ -17,15 +18,40 @@ Page({
     _loadData: function (callback) {
         merchantImageModel.getMerchantImages()
             .then(res => {
+                let images = this.getShowImages(res);
                 this.setData({
                     merchant_images: res,
-                    loadingCenter: false
+                    loadingCenter: false,
+                    showImageList: images
                 })
                 callback && callback();
             })
             .catch(res => {
                 console.log(res);
             })
+    },
+    
+    /**
+     * 点击图片预览
+     * @param {event} e 触发事件元素
+     */
+    showImage(e){
+        wx.previewImage({
+            current:e.target.dataset.pic,
+            urls:this.data.showImageList
+        })
+    },
+
+    /**
+     * 生成符合展示图片的数组
+     * @param {Array<object>} list 获取的图片数组 
+     */
+    getShowImages(list){
+        let res = [];
+        for(let i = 0;i<list.length;i++){
+            res.push(list[i].pic);
+        }
+        return res;
     },
 
     onPullDownRefresh: function () {
